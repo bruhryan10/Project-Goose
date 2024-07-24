170ab3a2c51523c15bfac9ccc57fbfded37cef71
Ryan <36418964+bruhryan10@users.noreply.github.com>
2024-07-23 19:20:06 -0700

    fixed game

--- Goose_Unity/Assets/!Scripts/EventManager.cs
+++ Goose_Unity/Assets/!Scripts/EventManager.cs
@@ -37,6 +37,6 @@ public class EventManager : MonoBehaviour
 
     void Update()
     {
-        
+
     }
 }
--- Goose_Unity/Assets/!Scripts/SceneLoader.cs
+++ Goose_Unity/Assets/!Scripts/SceneLoader.cs
@@ -12,7 +12,7 @@ public class SceneLoader : MonoBehaviour
 
     void Update()
     {
-        
+
     }
     public void QuitGame()
     {
